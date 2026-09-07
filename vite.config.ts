e275816790dc5d5ca8c8455209fb895e73d943cc
Ahmed Noor E Alam <ahmednooorr@gmail.com>
2026-09-07 20:07:44 +0200

    fix; replace / with ./ in base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.BASE_PATH || '/',
+  base: process.env.BASE_PATH || './',
   plugins: [react(), tailwindcss()],
 })
